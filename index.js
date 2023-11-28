eb0d5e90cfeb66f09636bd0a6dde2b942df0acbf
= <=>
2023-11-28 11:00:27 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+//Exercise 5:  Write a program that determines if a person is eligible to vote based on their age.
+const age = 18;
+if (age >= 18) {
+    console.log("You are eligible to vote!");
+}
+else {
+    console.log("Sorry, you are not eligible to vote yet.");
+}
---
+++ index.ts
@@ -0,0 +1,7 @@
+//Exercise 5:  Write a program that determines if a person is eligible to vote based on their age.
+const age: number = 18;
+if (age >= 18) {
+  console.log("You are eligible to vote!");
+} else {
+  console.log("Sorry, you are not eligible to vote yet.");
+}
